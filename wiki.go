0d298a6d52efe00cbb256992e6d3394e9ba78555
zwang <wzcwts521@gmail.com>
2018-01-25 16:04:15 -0800

    class03 use net/http package, something popped up. in mac you could you any case for testPage.txt file as title. linux not

--- wiki.go
+++ wiki.go
@@ -29,8 +29,8 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+func viewHandler(w http.ResponseWriter, z *http.Request) {
+	title := z.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
